f6910144f8bc099603f5292abf1c34128d16f4f6
Felipe Gomes Duarte <felipe.duarte@cortex-intelligence.com>
2022-09-01 20:58:17 -0300

    twitter_transform em dag.

--- airflow/dags/twitter_dag.py
+++ airflow/dags/twitter_dag.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from airflow.models import DAG
 from airflow.operators.alura import TwitterOperator
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from os.path import join
 
 
@@ -14,4 +15,24 @@ with DAG(dag_id="twitter_dag", start_date=datetime.now()) as dag:
             "extract_date={{ ds }}",
             "AluraOnline_{{ ds_nodash }}.json"
         )
+    )
+
+    twitter_transform = SparkSubmitOperator(
+        task_id='transform_twitter_aluraonline',
+        application=(
+            '/home/felipe/projetos/estudos/datapipeline/spark/transformation.py'
+        ),
+        name='twitter_transformation',
+        application_args=[
+            "--src",
+            "/home/felipe/projetos/estudos/datapipeline/datalake/bronze/twitter_aluraonline/extract_date=2022-08-25",
+
+            "--dest",
+            "/home/felipe/projetos/estudos/datapipeline/"
+            "datalake/silver/twitter_aluraonline",
+
+            "--process-date",
+            "{{ ds }}"
+        ]
+
     )
